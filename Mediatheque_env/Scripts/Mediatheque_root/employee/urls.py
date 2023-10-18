c039267655b9c750f4b2f61193d705a87de56e81
Kevin Rognon <rognonk@gmail.com>
2023-10-18 17:56:48 +0200

    Mise à jour

--- Mediatheque_env/Scripts/Mediatheque_root/employee/urls.py
+++ Mediatheque_env/Scripts/Mediatheque_root/employee/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('employees/', views.employee, name="employee"),
     path('employees/show-members/', views.show_members, name="show_members"),
     path('employees/show-medias/', views.show_medias, name="show_medias"),
-    path('employees/create-member/', views.create_member, name="create_member")
+    path('employees/create-member/', views.create_member, name="create_member"),
+    path('employees/delete-member/<int:member_id>/', views.delete_member, name="delete_member")
 ]
 
