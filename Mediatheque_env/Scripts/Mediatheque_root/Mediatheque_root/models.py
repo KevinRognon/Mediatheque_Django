7567f1a0753ab042cc1c70f889a4a8e84090a25a
Kevin Rognon <rognonk@gmail.com>
2023-10-18 15:33:02 +0200

    Mise à jour

---
+++ Mediatheque_env/Scripts/Mediatheque_root/Mediatheque_root/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=60)
+    dateBorrow = models.DateField(null=True)
+    available = models.BooleanField(null=False)
+    borrower = models.CharField(max_length=60)
+
+
+class Cd(models.Model):
+    name = models.CharField(max_length=100)
+    artist = models.CharField(max_length=60)
+    dateBorrow = models.DateField(null=False)
+    available = models.BooleanField(null=False)
+    borrower = models.CharField(max_length=60)
+
+
+class Dvd(models.Model):
+    name = models.CharField(max_length=100)
+    realisator = models.CharField(max_length=60)
+    dateBorrow = models.DateField(null=False)
+    available = models.BooleanField(null=False)
+    borrower = models.CharField(max_length=60)
+
+
+class BoardGame(models.Model):
+    name = models.CharField(max_length=100)
+    creator = models.CharField(max_length=60)
